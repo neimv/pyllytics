69ae2e33775cab77e37b50ecb403e268cfa5f7a2
neimv <neimv.zatara@gmail.com>
2022-01-18 23:03:47 -0600

    add compose

--- backend/N/N/settings.py
+++ backend/N/N/settings.py
@@ -79,9 +79,12 @@ WSGI_APPLICATION = 'N.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'neimv',
+        'PASSWORD': 'V:67012:h',
+        'NAME': 'pyllytics',
+        'HOST': 'localhost'
+    },
 }
 
 
